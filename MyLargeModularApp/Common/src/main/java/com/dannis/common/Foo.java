e10e457a5d044084db49344716f33c92850da7fa
foo <foo@example.com>
2017-06-16 17:43:36 +0800

    common project add.

---
+++ MyLargeModularApp/Common/src/main/java/com/dannis/common/Foo.java
@@ -0,0 +1,13 @@
+package com.dannis.common;
+
+/**
+ * Created by acadsoc on 2017-06-16.
+ */
+public class Foo {
+    public String name = null;
+
+    public void bar() {
+        System.out.println("Common项目Foo.bar().被调用!!!");
+    }
+
+}
--- MyLargeModularApp/MyModuleJar/src/main/java/net/srirangan/packt/maven/App.java
+++ MyLargeModularApp/MyModuleJar/src/main/java/net/srirangan/packt/maven/App.java
@@ -1,13 +1,14 @@
 package net.srirangan.packt.maven;
 
+import com.dannis.common.Foo;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Foo foo = new Foo();
+        foo.bar();
+        System.out.println("Hello World! 你好，世界!");
     }
 }
