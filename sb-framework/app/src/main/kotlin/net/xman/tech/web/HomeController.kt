17d570cd0a6276bae9a9b518f5e1c1aa4dacbcdd
cgwu <gg.wcg@qq.com>
2017-06-21 23:21:52 +0800

    Redis Cache added not tested.

--- sb-framework/app/src/main/kotlin/net/xman/tech/web/HomeController.kt
+++ sb-framework/app/src/main/kotlin/net/xman/tech/web/HomeController.kt
@@ -1,16 +1,29 @@
 package net.xman.tech.web
 
 import net.xman.tech.common.domain.UserEntity
+import net.xman.tech.service.CachedService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
 
 
 @RestController
 class HomeController {
+    @Autowired
+    private var cachedService: CachedService? = null
+
     @RequestMapping("/")
-    fun Index(): String {
+    fun index(): String {
         val u = UserEntity("李四", 44, "000111")
         return "${LocalDateTime.now()}: Hello Kotlink App!! 欢迎你来到app: $u"
     }
+
+    @RequestMapping("/cache")
+    fun cache(@RequestParam i:Int): String {
+        val result = cachedService!!.computePiDecimal(i)
+        return "computePiDecimal: $result"
+    }
+
 }
