43ec2f39aff7f4f3619f776983b346b825906307
cgwu <gg.wcg@qq.com>
2017-06-21 22:07:04 +0800

    test rest webclient.

--- sb-framework/app/src/main/kotlin/net/xman/tech/service/HelloRestServiceImpl1.kt
+++ sb-framework/app/src/main/kotlin/net/xman/tech/service/HelloRestServiceImpl1.kt
@@ -2,6 +2,7 @@ package net.xman.tech.service
 
 import io.swagger.annotations.Api
 import net.xman.tech.common.service.HelloRestService
+import org.slf4j.LoggerFactory
 
 /**
  * Created by sam on 17-6-19.
@@ -9,7 +10,10 @@ import net.xman.tech.common.service.HelloRestService
  */
 @Api("/sayHello")
 class HelloRestServiceImpl1 : HelloRestService {
+    private val log = LoggerFactory.getLogger(HelloRestServiceImpl1::class.java)
+
     override fun sayHello(a: String): String {
+        log.info("接收到Rest请求sayHello: $a")
         return "Hello $a, Welcome to CXF RS Spring Boot World!!!"
     }
 }
