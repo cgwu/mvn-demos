ab988a46011bda61c3f494f2f23c403ca621a38e
foo <foo@example.com>
2017-06-22 19:34:27 +0800

    redis config added.

--- sb-framework/app/src/main/kotlin/net/xman/tech/config/RedisConfig.kt
+++ sb-framework/app/src/main/kotlin/net/xman/tech/config/RedisConfig.kt
@@ -1,10 +1,65 @@
 package net.xman.tech.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.annotation.JsonAutoDetect
+import com.fasterxml.jackson.annotation.PropertyAccessor
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
+import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.CachingConfigurerSupport
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.interceptor.KeyGenerator
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.lang.reflect.Method
+
+
 /**
  * Created by sam on 17-6-21.
  * TODO: Redis缓存使用
  * 参考: http://www.cnblogs.com/ityouknow/p/5748830.html
  */
-class RedisConfig {
+@Configuration
+@EnableCaching
+class RedisConfig : CachingConfigurerSupport() {
+
+    @Bean
+    override fun keyGenerator(): KeyGenerator {
+        return object : KeyGenerator {
+            override fun generate(target: Any, method: Method, vararg params: Any): Any {
+                val sb = StringBuilder()
+                sb.append(target.javaClass.name)
+                sb.append(method.getName())
+                for (obj in params) {
+                    sb.append(obj.toString())
+                }
+                return sb.toString()
+            }
+        }
+    }
+
+    @Bean
+    fun cacheManager(redisTemplate: RedisTemplate<*, *>): CacheManager {
+        val rcm = RedisCacheManager(redisTemplate)
+        //设置缓存过期时间
+        //rcm.setDefaultExpiration(60);//秒
+        return rcm
+    }
+
+    @Bean
+    fun redisTemplate(factory: RedisConnectionFactory): RedisTemplate<String, String> {
+        val template = StringRedisTemplate(factory)
+        val jackson2JsonRedisSerializer = Jackson2JsonRedisSerializer(Any::class.java)
+        val om = ObjectMapper()
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY)
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL)
+        jackson2JsonRedisSerializer.setObjectMapper(om)
+        template.valueSerializer = jackson2JsonRedisSerializer
+        template.afterPropertiesSet()
+        return template
+    }
 
 }
